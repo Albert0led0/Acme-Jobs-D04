3887a6018e50b18586b4b0f8b70baf3063d2d939
mpg97 <migpatgut@alum.us.es>
2019-11-29 04:29:44 +0100

    Task 006: not finished. pending check

--- src/main/java/acme/entities/banners/Banner.java
+++ src/main/java/acme/entities/banners/Banner.java
@@ -2,10 +2,14 @@
 package acme.entities.banners;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.URL;
 
+import acme.entities.roles.Sponsor;
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,4 +35,14 @@ public abstract class Banner extends DomainEntity {
 	@URL
 	@NotBlank
 	private String				targetURL;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Valid
+	@NotNull
+	@ManyToOne(optional = false)
+	private Sponsor				sponsor;
+
 }
